ce222d7b06ba516cc206f15cb14e6cebcbc813e4
Shailesh Kumar <shailesh7492966727@gmial.com>
2024-12-20 23:51:12 +0530

    Design UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
+import styles from "./App.module.css";
+import Container from "./Components/Container";
+import Display from "./Components/Display";
+import ButtonContainer from "./Components/ButtonContainer";
+
 
 function App() {
   
   return (
     <>
-      
+    <Container>
+      <div className={styles.calculator}>
+        <Display></Display>
+        <ButtonContainer></ButtonContainer>
+      </div>
+      </Container>
     </>
   )
 }
